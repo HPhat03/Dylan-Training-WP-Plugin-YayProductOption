314e628b88ddba061bd1b362fefda5c1833de46b
HPhat03 <107537439+HPhat03@users.noreply.github.com>
2025-10-24 16:14:19 +0700

    init: Yay Product Schema type

---
+++ react-app/src/schema.ts
@@ -0,0 +1,22 @@
+import z from "zod"
+
+const choiceSchema = z.object({
+    title: z.string(),
+    value: z.string(),
+    rvalue: z.any()
+})
+const optionSchema =   z.object({
+    label: z.string(),
+    displayLabel: z.string(),
+    choices: z.array(choiceSchema),
+})
+const productSchema = z.object({
+    id: z.number(),
+    name: z.string()    
+}); 
+
+export const productIncludeSchema = z.object({
+    product: z.object(productSchema),
+    options: z.array(optionSchema)
+})
+
